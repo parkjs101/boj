804f47593085c6acaa70adec25981b9b4912121d
parkjs101 <trckstr4422@gmail.com>
2023-06-11 16:03:24 +0900

    changes

--- 14889.py
+++ 14889.py
@@ -1,52 +1,42 @@
+from collections import deque
 n = int(input())
+
 li = []
-arr = []
-value = [0,0]
-m = n*(n-1)//2
-isused = [0]*(n+1)
-minimum = []
+arr0 = [i for i in range(1, n+1)]
+arr = [1]
+isused = [False]*(n+1)
+minimum = [100000000]
 
 for i in range(n):
     li.append(list(map(int, input().split())))
 
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if i >= j:
-            continue
-        arr.append([i, j])
-
-print(arr)
-
-def func1(k, x):
+def func1(k):
+    global minimum
     if k == n//2:
+        value = [0, 0]
+        for i in range(1, n+1):
+            for j in range(i+1, n+1):
+                if i in arr and j in arr:
+                    value[0] += li[i-1][j-1] + li[j-1][i-1]
+                elif i not in arr and j not in arr:
+                    value[1] += li[i-1][j-1] + li[j-1][i-1]
         if value[0] > value[1]:
-            minimum.append(value[0]-value[1])
+            if minimum[0] > value[0]-value[1]:
+                minimum.pop()
+                minimum.append(value[0]-value[1])
         else:
-            minimum.append(value[1]-value[0])
-        return
-    else:
-        for i in range(n):
-            for j in range(n):
-                if i >= j or isused[i] != 0 or isused[j] != 0:
-                    continue
-                else:
-                    isused[i] = 1
-                    isused[j] = 1
-                    value[x] += li[i][j] + li[j][i]
-                    func1(k+1,1-x)
-                    value[x] -= li[i][j] + li[j][i]
-                    isused[i] = 0
-                    isused[j] = 0
-"""
-            if isused[arr[i][0]] == 0 and isused[arr[i][1]] == 0:
-                isused[arr[i][0]] = 1
-                isused[arr[i][1]] = 1
-                v = li[arr[i][0]-1][arr[i][1]-1] + li[arr[i][1]-1][arr[i][0]-1]
-                value[x] += v
-                func1(k+1,1-x)
-                value[x] -= v
-                isused[arr[i][0]] = 0
-                isused[arr[i][1]] = 0 """
+            if minimum[0] > value[1]-value[0]:
+                minimum.pop()
+                minimum.append(value[1]-value[0])
 
-func1(0, 0)
-print(min(minimum))
+    else:
+        for i in range(2, n+1):
+            if isused[i] != True and i > arr[-1]:
+                arr.append(i)
+                isused[i] = True
+                func1(k+1)
+                arr.pop()
+                isused[i] = False
+
+func1(1)
+print(minimum)

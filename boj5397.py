2c1e43da0aef23d9798d1ec612f8507b87042635
parkjs101 <trckstr4422@gmail.com>
2023-06-16 16:52:14 +0900

    Update boj5397.py

--- boj5397.py
+++ boj5397.py
@@ -1,32 +1,54 @@
+from collections import deque
+
 t = int(input())
 
 for i in range(t):
   s = input()
   p = 0
-  arr = ''
+  arr = deque()
   for i in range(len(s)):
     if s[i] == '<':
-      if p >0:
+      if p > 0:
         p -= 1
     elif s[i] == '>':
-      if p < len(s):
+      if p < len(arr):
         p += 1
     elif s[i] == '-':
-      if p == len(s):
-        arr = arr[:p-2]
+      if p == len(arr) and len(arr)>0:
+        arr.pop()
         p -= 1
       elif p > 0:
-        arr = arr[:p-2]+arr[p:]
+        newarr = deque()
+        for j in range(len(arr)+1):
+          if j < p-1:
+            newarr.append(arr.popleft())
+          elif j == p-1:
+            arr.popleft()
+          if j > p-1:
+            newarr.append(arr.popleft())
+        arr = newarr
         p -= 1
     else:
-      if p == len(s):
-        arr = arr+s[i]
+      if p == len(arr):
+        arr.append(s[i])
         p += 1
       elif p > 0:
-        arr = arr[p-1] + s[i] + arr[p:]
+        newarr = deque()
+        for j in range(len(arr)+1):
+          if j < p:
+            newarr.append(arr.popleft())
+          elif j == p:
+            newarr.append(s[i])
+          if j > p:
+            newarr.append(arr.popleft())
+        arr = newarr
         p += 1
       else:
-        arr = s[i]+arr
+        arr.appendleft(s[i])
         p += 1
-  
-  print(arr)
+
+  for i in range(len(arr)):
+    if i == len(arr)-1:
+      print(arr[i])
+    else:
+      print(arr[i],end='')

36e20bf9aaa60268cbe496336ef4490717e508f0
parkjs101 <trckstr4422@gmail.com>
2023-06-21 11:02:54 +0900

    Update boj2504.py

--- boj2504.py
+++ boj2504.py
@@ -5,22 +5,12 @@ def func1():
         return print(0)
     stack = []
     numstack = []
-    total = 0
     for i in range(len(array)):
+        stack.append(array[i])
         if array[i] == '(':
-            stack.append(array[i])
             numstack.append(2)
-        elif array[i] == '[':
-            stack.append(array[i])
-            numstack.append(3)
-        elif array[i] == ')':
-            if stack[-1] == '(':
-                if len(stack)>2 and stack[-2] == '(':
-                    numstack[-2] = numstack[-2]*numstack.pop()
-                stack.pop()
+            if array[i-1] == '(':
                 
-            elif stack[-1]
-            pass
-        elif array[i] == ']':
-            
-            pass
+        elif array[i] == '[':
+            numstack.append(3)
+        

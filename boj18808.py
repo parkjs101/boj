d09c3b39081d7e53ac2da9a05486b528c567f7de
parkjs101 <trckstr4422@gmail.com>
2023-06-30 16:58:38 +0900

    Update boj18808.py

--- boj18808.py
+++ boj18808.py
@@ -2,6 +2,7 @@ n, m, k = map(int, input().split()) # k = 스티커 개수
 
 board = [[0 for _ in range(m)] for _ in range(n)]
 sticker = [0]*k #스티커 정보 저장
+count = 0
 
 for l in range(k):
   sticker_n, sticker_m = map(int, input().split())
@@ -9,23 +10,52 @@ for l in range(k):
   for i in range(sticker_n):
     sticker[l][i] = (list(map(int, input().split())))
 
-print(sticker)
+def rotate_sticker(l):
+  new_sticker = [[0 for _ in range(len(sticker[l]))] for _ in range(len(sticker[l][0]))]
+  for i in range(len(sticker[l])):
+    for j in range(len(sticker[l][0])):
+      new_sticker[j][len(sticker[l])-i-1] = sticker[l][i][j]
+  sticker[l] = new_sticker
 
-for l in range(k):
-  cur_sticker = sticker[l]
-  for _ in range(4):
-    for i in range(n):
-      for j in range(m):
-        cur_x = 0
-        cur_y = 0
-        while True:
-          if cur_sticker[cur_y][cur_x] == 0 and sticker[n+cur_y][m+cur_x] == 0:
-            pass
-          elif cur_sticker[cur_y][cur_x] == 1 and sticker[n+cur_y][m+cur_x] == 0:
-            pass
-          elif cur_sticker[cur_y][cur_x] == 1 and sticker[n+cur_y][m+cur_x] == 1:
-            flag = False
+for l in range(k): #스티커 개수
+  for _ in range(4): #방향
+    b_flag = False #보드
+    for i in range(n-len(sticker[l])+1):
+      for j in range(m-len(sticker[l][0])+1):
+        s_flag = True #스티커
+        is_sticked = False
+        for o in range(len(sticker[l])): 
+          for p in range(len(sticker[l][0])):
+            if sticker[l][o][p] == 0:
+              continue
+            elif sticker[l][o][p] == 1 and board[i+o][j+p] != 0:
+              s_flag = False
+              break
+            elif sticker[l][o][p] == 1 and board[i+o][j+p] == 0:
+              board[i+o][j+p] = l+1
+              is_sticked = True
+          if s_flag == False:
             break
-          elif cur_sticker[cur_y][cur_x] == 0 and sticker[n+cur_y][m+cur_x] == 1:
-            flag = False
-            break
+        if s_flag == True:
+          b_flag = True
+          break
+        else:
+          if is_sticked == True:
+            for a in range(n):
+              for b in range(m):
+                if board[a][b] == l+1:
+                  board[a][b] = 0
+ 
+      if b_flag == True:
+        break
+    if b_flag == True:
+      break
+    else:
+      rotate_sticker(l)
+
+for i in range(n):
+  for j in range(m):
+    if board[i][j] != 0:
+      count += 1
+
+print(count)
